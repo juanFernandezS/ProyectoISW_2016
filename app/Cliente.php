0ca0538727c58b31467d13ebc9db59c5635b5821
Ale Arias <manarias@alumnos.ubiobio.cl>
2016-11-08 16:44:43 -0300

    creacion de relaciones cliente y zona

--- app/Cliente.php
+++ app/Cliente.php
@@ -8,4 +8,9 @@ class Cliente extends Model
 {
     protected $table="clientes";
     protected $fillable=['id','telefono','nombre','descripcion','id_zona'];
+
+    public function zona(){
+
+        return $this->belongsTo('App\Zona','id_zona');
+    }
 }
--- app/Zona.php
+++ app/Zona.php
@@ -8,4 +8,10 @@ class Zona extends Model
 {
     protected $table= "zonas";
     protected $fillable=['id','nombre'];
+
+
+    public function cliente(){
+
+        return $this->hasMany('App\Cliente', 'id_zona');
+    }
 }
