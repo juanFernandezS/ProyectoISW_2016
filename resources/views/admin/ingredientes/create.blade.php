1afa83c227ecba674d41f65c2f05ce99f4bf7df5
Javiera Rain <javiera.rain1401@alumnos.ubiobio.cl>
2016-11-10 13:29:58 -0300

    crear

---
+++ resources/views/admin/ingredientes/create.blade.php
@@ -0,0 +1,19 @@
+@extends('admin.template.main')
+
+@section('titulo','agregar ingrediente')
+
+@section('content')
+    {!! Form::open(['route' => 'admin.ingrediente.store','method' =>'POST']) !!}
+    <div class="form-group">
+        {!! Form::label('nombre','Nombre ingrediente') !!}
+        {!! Form::text('nombre',null,['class' =>'form-control','placeholder'=>'Ingresar nombre']) !!}
+    </div>
+
+    <div class="form-group">
+
+        {!! Form::submit('registrar',['class' => 'btn btn-primary'])!!}
+
+    {!! Form::close() !!}
+
+@endsection
+
