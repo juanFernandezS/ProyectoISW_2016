e134aec23d5b5aa1e3c5920e2faabadf3a4bbcdd
juane <juan.e.fernandez.suazo@gmail.com>
2016-11-10 16:03:09 -0300

    termino creacion de una Cobertura

--- resources/views/admin/coberturas/create.blade.php
+++ resources/views/admin/coberturas/create.blade.php
@@ -1,3 +1,24 @@
 @extends('admin.template.main')
-@extends('titulo','lista coberturas')
 
+@section('titulo', 'agregar coberturas')
+
+@section('content')
+    {!! Form::open(['route' => 'admin.coberturas.store', 'method' => 'POST']) !!}
+
+    <div class="form-group">
+        {!!Form::label('name','Nombre') !!}
+        {!!Form::text('name', null, ['class'=> 'form-control', 'placeholder' => 'Nombre de la cobertura', 'require']) !!}
+    </div>
+
+    <div class="form-group">
+        {!!Form::label('precio','Precio($)') !!}
+        {!!Form::text('precio', null, ['class'=> 'form-control', 'placeholder' => '10000', 'require']) !!}
+    </div>
+
+    <div class="form-group">
+        {!!Form::submit('registrar', ['class' => 'btn btn-primary']) !!}
+    </div>
+
+   {!! Form::close()!!}
+
+@endsection
