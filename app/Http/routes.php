6d26d40919a4853cc1d57c5c72d6834244492aec
juane <juan.e.fernandez.suazo@gmail.com>
2016-11-10 12:02:42 -0300

    Creo ruta para coberturas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admg'], function (){
+
+    Route::resource('coberturas','CoberturasController');
+
+});
