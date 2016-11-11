3c775559c7e17c2014d003eb827ffc75e516155d
juane <juan.e.fernandez.suazo@gmail.com>
2016-11-11 13:45:33 -0300

    Fixeado error al mandar de campo nombre al controlador

--- resources/views/admin/coberturas/create.blade.php
+++ resources/views/admin/coberturas/create.blade.php
@@ -6,8 +6,8 @@
     {!! Form::open(['route' => 'admin.coberturas.store', 'method' => 'POST']) !!}
 
     <div class="form-group">
-        {!!Form::label('name','Nombre') !!}
-        {!!Form::text('name', null, ['class'=> 'form-control', 'placeholder' => 'Nombre de la cobertura', 'require']) !!}
+        {!!Form::label('nombre','Nombre') !!}
+        {!!Form::text('nombre', null, ['class'=> 'form-control', 'placeholder' => 'Nombre de la cobertura', 'require']) !!}
     </div>
 
     <div class="form-group">
