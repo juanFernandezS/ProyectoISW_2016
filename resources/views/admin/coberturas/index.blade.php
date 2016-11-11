0251fe21f30242483c6fda7d1c539c0f40b5ccc1
juane <juan.e.fernandez.suazo@gmail.com>
2016-11-11 13:46:19 -0300

    Agrega boton de borrar y agregar una nueva cobertura

--- resources/views/admin/coberturas/index.blade.php
+++ resources/views/admin/coberturas/index.blade.php
@@ -3,11 +3,13 @@
 @section('titulo','lista de coberturas')
 
 @section('content')
+    <a href="{{ route('admin.coberturas.create') }}" class="btn btn-info">Ingresar nueva cobertura</a>
     <table class="table" table-striped>
         <thead>
             <th>ID</th>
             <th>Nombre Cobertura</th>
             <th>Precio</th>
+            <th>Acción</th>
         </thead>
         <tbody>
             @foreach($coberturas as $cobertura)
@@ -15,9 +17,14 @@
                     <td>{{ $cobertura->id }}</td>
                     <td>{{ $cobertura->nombre }}</td>
                     <td>{{ $cobertura->precio }}</td>
+
+                    <td><a href="" class="btn btn-danger"><span class="glyphicon glyphicon-wrench" aria-hidden="true"></span></a>
+                        <a href=" {{ route('admin.coberturas.destroy', $cobertura->id) }}" class="btn btn-warning" onclick="return confirm('seguro que quieres eliminarlo?')"><span class="glyphicon glyphicon-remove-circle" aria-hidden="true"></span></a></td>
                 </tr>
             @endforeach
         </tbody>
     </table>
+
+    {!! $coberturas->render() !!}
 
 @endsection
