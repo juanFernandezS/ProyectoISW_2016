ad863a133d6d63233d30226131daa31676f9f821
Ale Arias <manarias@alumnos.ubiobio.cl>
2016-11-13 21:50:03 -0300

    validation all instances working

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -19,7 +19,7 @@ class ClienteController extends Controller
      */
     public function index()
     {
-        $cliente = Cliente::orderBy('id','ASC')->paginate(5);
+        $cliente = Cliente::orderBy('id','ASC')->paginate(10);
         return view ('commonusers.index')->with('cliente',$cliente);
 
     }
