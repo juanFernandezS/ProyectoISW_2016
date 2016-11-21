17f9e46ddab05e4f8ec974d3c3228873b4c83848
basrodri <basrodri@alumnos.ubiobio.cl>
2016-11-21 09:39:12 -0300

    autenticacion fixed 100% real no fake

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -49,6 +49,6 @@ Route::get('/home', 'HomeController@home');
 
 Route::get('auth/login','Auth\AuthController@getLogin');
 Route::post('auth/login','Auth\AuthController@postLogin');
-Route::get('auth/logout','Auth\AuthController@getLogout');
+Route::get('auth/logout','Auth\AuthController@logout');
 
 
