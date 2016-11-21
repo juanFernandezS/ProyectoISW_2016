363bcaf72444950b3b8e592d2b12365ad5a14e1e
juane <juan.e.fernandez.suazo@gmail.com>
2016-11-21 12:36:00 -0300

    editado controlador coberturas:: mensaje eliminar version:1

--- resources/views/admin/coberturas/index.blade.php
+++ resources/views/admin/coberturas/index.blade.php
@@ -3,6 +3,15 @@
 @section('Coberturas','lista de coberturas')
 
 @section('content')
+
+
+    <script>
+        function mensaje_eliminar($cobertura){
+            //return "seguro que quieres eliminar\n**nombre: **\n**precio: ";
+            return "seguro que quieres eliminar\n**nombre: ",+$cobertura->nombre,"**\n**precio: ",+$cobertura->precio,"";
+        }
+    </script>
+
     <a href="{{ route('admin.coberturas.create') }}" class="btn btn-info">Ingresar nueva cobertura</a><hr>
     <table class="table" table-striped>
         <thead>
@@ -11,7 +20,9 @@
             <th>Precio</th>
             <th>Acción</th>
         </thead>
+
         <tbody>
+
             @foreach($coberturas as $cobertura)
                 <tr>
                     <td>{{ $cobertura->id }}</td>
@@ -19,7 +30,7 @@
                     <td>{{ $cobertura->precio }}</td>
 
                     <td><a href=" {{ route('admin.coberturas.edit', $cobertura->id) }} " class="btn btn-warning"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span></a>
-                        <a href=" {{ route('admin.coberturas.destroy', $cobertura->id) }}" class="btn btn-danger" onclick="return confirm('seguro que quieres eliminarlo?')"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></a></td>
+                        <a href=" {{ route('admin.coberturas.destroy', $cobertura->id) }}" class="btn btn-danger" onclick= "return confirm(mensaje_eliminar($cobertura))"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></a></td>
                 </tr>
             @endforeach
         </tbody>
