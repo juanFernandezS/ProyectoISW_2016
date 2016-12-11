b278139538ff0e2f739c2e779c737a094be9ba8d
Javiera Rain <javiera.rain1401@alumnos.ubiobio.cl>
2016-12-11 20:36:10 -0300

    test

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(EstadoTableSeeder::class);
         $this->call(UsersTableSeeder::class);
+        $this->call(PagosTableSeeder::class);
     }
 }
