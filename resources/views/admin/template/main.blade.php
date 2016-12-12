5fecfe4ad6108d505384eb5d97e155f24d58319d
juane <juan.e.fernandez.suazo@gmail.com>
2016-12-12 10:10:01 -0300

    fixeo de barra

--- resources/views/admin/template/main.blade.php
+++ resources/views/admin/template/main.blade.php
@@ -71,7 +71,7 @@ desired effect
     <header class="main-header">
 
         <!-- Logo -->
-        <a href="#" class="logo">
+        <a href="{{ URL::to('#') }}" class="logo">
             <!-- mini logo for sidebar mini 50x50 pixels -->
             <span class="logo-mini"><b>S</b>S</span>
             <!-- logo for regular state and mobile devices -->
